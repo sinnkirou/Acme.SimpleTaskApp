a14bab190379db711fe74da0b80f44ea35ba1eb4
ziv-zhao <ziv.zhao@perficient.com>
2018-10-10 11:02:37 +0800

    first commit

---
+++ src/Acme.SimpleTaskApp.EntityFrameworkCore/EntityFrameworkCore/SimpleTaskAppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Abp.EntityFrameworkCore;
+using Acme.SimpleTaskApp.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Acme.SimpleTaskApp.EntityFrameworkCore
+{
+    public class SimpleTaskAppDbContext : AbpDbContext
+    {
+
+        public SimpleTaskAppDbContext(DbContextOptions<SimpleTaskAppDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
