a16a18f9c1fcaf65b7621cc777228a9d76c146bf
ziv-zhao <ziv.zhao@perficient.com>
2018-10-10 16:38:32 +0800

    remove About and Home tabs

--- src/Acme.SimpleTaskApp.Web/Startup/PageNames.cs
+++ src/Acme.SimpleTaskApp.Web/Startup/PageNames.cs
@@ -2,8 +2,6 @@
 {
     public class PageNames
     {
-        public const string Home = "Home";
-        public const string About = "About";
         public const string TaskList = "TaskList";
     }
 }
